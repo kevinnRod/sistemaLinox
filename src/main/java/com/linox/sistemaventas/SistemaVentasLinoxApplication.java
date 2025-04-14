c5bc51cf90dbd9dfdbb36bf6a0763c9018ee7cdc
kevinnRod <kevinrodp19@gmail.com>
2025-04-14 15:05:26 -0500

    Primer commit del proyecto

---
+++ src/main/java/com/linox/sistemaventas/SistemaVentasLinoxApplication.java
@@ -0,0 +1,13 @@
+package com.linox.sistemaventas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaVentasLinoxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaVentasLinoxApplication.class, args);
+	}
+
+}
