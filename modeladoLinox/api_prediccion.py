1dda77e0bdb3591215098be144abcba84a09e85b
kevinRod_ <kevinrodp19@gmail.com>
2025-07-12 00:04:59 -0500

    Acualizacion con el puerto 5000

--- modeladoLinox/api_prediccion.py
+++ modeladoLinox/api_prediccion.py
@@ -76,4 +76,4 @@ def prediccion_mes():
 
 if __name__ == '__main__':
     print("Flask se está ejecutando desde __main__")
-    app.run(debug=False, use_reloader=False, host='0.0.0.0', port=5050)
+    app.run(debug=False, use_reloader=False, host='0.0.0.0', port=5000)
